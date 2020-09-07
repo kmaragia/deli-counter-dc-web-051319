53e2661045df35aab950fee36cde847d5848e2e9
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-07 17:42:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+      puts "The line is currently: #{katz_deli.each do |x|
+        puts "#{katz_deli.index(x)}#{x}"
+      }"
+  end
+end
+
+def take_a_number(katz_deli,str)
+  puts "Welcome, #{str}. You are number #{katz_deli.length +1} in line."
+  katz_deli << str
+end
