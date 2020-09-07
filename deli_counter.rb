a087ffc2c4e8ba9dd9164a85e5addab92f1a840e
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-07 17:42:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,7 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-      puts "The line is currently: #{katz_deli.each do |x|
-        puts "#{katz_deli.index(x)}#{x}"
-      }"
+      puts "The line is currently:"
   end
 end
 
